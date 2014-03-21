3f4848c36c0a692091857eb566466d36a98598cf
L7D <smhjyh2009@gmail.com>
2014-03-21 21:52:06 +0900

    Config document add
    L7D - :)

--- lua/autorun/LShop/sh/sh_config.lua
+++ lua/autorun/LShop/sh/sh_config.lua
@@ -1,20 +1,22 @@
 LShop.Config = LShop.Config or {}
 
-LShop.Config.Version = "1.2A" -- Do not edit this!
+LShop.Config.Version = "1.2" -- Do not edit this!
 
 LShop.Config.OpenCommand = "LShop_open" -- Set menu open console command.
 LShop.Config.OpenKey = "F4" -- Set menu open key.
 LShop.Config.ChatOpenCommand = "/XPSys_open" -- Set menu open chat command.
 LShop.Config.ChatOpenCommand_Output = "" -- Set menu open chat command output string.
---[[
-	= LShop.Config.OpenKey List =
-	F1 
-	F2
-	F3
-	F4
---]]
 
-LShop.Config.AutoMoneyGive = true -- Are you sure turn on Auto Money Give system?
+--[[// #################################### //
+	= Shop Open Key Document =
+	F1 -- Yeah F1 Button. :)
+	F2 -- Yeah F2 Button. :)
+	F3 -- Yeah F3 Button. :)
+	F4 -- Yeah F4 Button. :)
+--]]// #################################### //
+
+
+LShop.Config.AutoMoneyGive = true -- Are you sure turn on Auto Money Give system? ( Timer system )
 LShop.Config.MoneyGiveTimer = 60 -- ( Sec )
 LShop.Config.MoneyAmmount = 100 -- Money Amount. ( 100 $ )
 
@@ -23,3 +25,29 @@ LShop.Config.ItemGiftSystem = true -- Are you sure allow Item Gift System?
 LShop.Config.PermissionCheck = function( pl ) -- Administrator menu permission.
 	return pl:IsSuperAdmin()
 end
+
+--[[// #################################### //
+	= Administrator Permission Check System Document =
+
+	-- Give permission super admins!
+	LShop.Config.PermissionCheck = function( pl )
+		return pl:IsSuperAdmin()
+	end
+	
+	-- Give permission admins!
+	LShop.Config.PermissionCheck = function( pl )
+		return pl:IsAdmin()
+	end
+	
+	-- Give permission only that Steam ID!
+	// NOTICE : If you want get your Steam ID? use this site!
+	// http://steamidfinder.com/
+	
+	LShop.Config.PermissionCheck = function( pl )
+		if ( pl:SteamID() == "STEAM ID HERE" ) then
+			return true
+		else
+			return false
+		end
+	end
+--]]// #################################### //
