2da1df8cfbb4b8b8627cc4e40779ddd5840ce7c0
L7D <smhjyh2009@gmail.com>
2014-03-24 23:11:11 +0900

    Typo error fix
    L7D - :)

--- lua/autorun/LShop/sh/sh_config.lua
+++ lua/autorun/LShop/sh/sh_config.lua
@@ -4,7 +4,7 @@ LShop.Config.Version = "1.3" -- Do not edit this!
 
 LShop.Config.OpenCommand = "LShop_open" -- Set menu open console command.
 LShop.Config.OpenKey = "F4" -- Set menu open key.
-LShop.Config.ChatOpenCommand = "/XPSys_open" -- Set menu open chat command.
+LShop.Config.ChatOpenCommand = "/LShop_open" -- Set menu open chat command.
 LShop.Config.ChatOpenCommand_Output = "" -- Set menu open chat command output string.
 
 --[[// #################################### //
